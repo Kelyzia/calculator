df661ef32abbb060269db5a811928a2864553c8d
Teresia Gable <teresia.gable@gmail.com>
2018-12-12 09:52:04 +0100

    first commit

---
+++ src/main/java/se/lexicon/teresia/calculator_assignment/App.java
@@ -0,0 +1,13 @@
+package se.lexicon.teresia.calculator_assignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
